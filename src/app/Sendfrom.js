d2c3d481654e62085e4979bb930b79fd12e91670
sharif780 <srka780@gmail.com>
2024-08-28 18:01:38 +0600

    htmlfor react

--- src/app/Sendfrom.js
+++ src/app/Sendfrom.js
@@ -34,26 +34,26 @@ export default function Sendfrom () {
     return (
         <form onSubmit={sendproduct} className="max-w-sm mx-auto">
           <div className="mb-5">
-            <label for="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">title</label>
+            <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">title</label>
             <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="title" required />
           </div>
           <div className="mb-5">
-            <label for="stock" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">stock</label>
+            <label htmlFor="stock" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">stock</label>
             <input type="number" id="stock" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
 
 
           <div className="mb-5">
-            <label for="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">price</label>
+            <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">price</label>
             <input type="number" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
           <div className="mb-5">
-            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">email</label>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">email</label>
             <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
 
           <div className="mb-5">
-            <label for="discount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">discount</label>
+            <label htmlFor="discount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">discount</label>
             <input type="number" id="discount" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
          
